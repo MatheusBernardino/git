0c887b21e85a1c83d38a3992c55576a19632bf40
Matheus Tavares <matheus.bernardino@usp.br>
2019-03-24 18:23:14 -0300

    PROVISORY
    Testing travis doc builds

--- ci/test-documentation.sh
+++ ci/test-documentation.sh
@@ -11,20 +11,8 @@ gem install asciidoctor
 make check-builtins
 make check-docs
 
-# Build docs with AsciiDoc
-make doc > >(tee stdout.log) 2> >(tee stderr.log >&2)
-! test -s stderr.log
-test -s Documentation/git.html
-test -s Documentation/git.xml
-test -s Documentation/git.1
-grep '<meta name="generator" content="AsciiDoc ' Documentation/git.html
-
-rm -f stdout.log stderr.log
-check_unignored_build_artifacts
-
 # Build docs with AsciiDoctor
-make clean
-make USE_ASCIIDOCTOR=1 doc > >(tee stdout.log) 2> >(tee stderr.log >&2)
+make V=1 USE_ASCIIDOCTOR=1 doc > >(tee stdout.log) 2> >(tee stderr.log >&2)
 sed '/^GIT_VERSION = / d' stderr.log
 ! test -s stderr.log
 test -s Documentation/git.html
