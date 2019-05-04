1f9945080bbd29ff8c1d867839d4594f30ac4934
Matheus Tavares <matheus.bernardino@usp.br>
2019-05-03 23:38:30 -0300

    ci: compile and run tests using parallel jobs
    Use all the available cores to perform a parallel compilation and to run
    tests, setting make's '-j' option.
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -10,8 +10,12 @@ windows*) cmd //c mklink //j t\\.prove "$(cygpath -aw "$cache_dir/.prove")";;
 *) ln -s "$cache_dir/.prove" t/.prove;;
 esac
 
-make
-make test
+CORES=2
+
+echo "Compiling and running tests with $CORES jobs"
+
+make -j$CORES
+make -j$CORES test
 if test "$jobname" = "linux-gcc"
 then
 	export GIT_TEST_SPLIT_INDEX=yes
@@ -20,7 +24,7 @@ then
 	export GIT_TEST_OE_DELTA_SIZE=5
 	export GIT_TEST_COMMIT_GRAPH=1
 	export GIT_TEST_MULTI_PACK_INDEX=1
-	make test
+	make -j$CORES test
 fi
 
 check_unignored_build_artifacts
