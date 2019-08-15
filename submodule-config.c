9bfbdb2aa1ba653df15d49e7ff4ff3fabd514a03
Matheus Tavares <matheus.bernardino@usp.br>
2019-08-15 20:28:52 -0300

    config: add repo_config_with_options()
    Because config_with_options() relies on the global the_repository, a
    callee that wants to work on a subrepo must first add it to alternates.
    This can negatively affect performance and memory. So let's rename and
    modify it to take a struct repository as parameter and pass it down in
    the call stack. Also add an alias config_with_options() which uses the
    global the_repository by default, for backwards compatibility.
    
    The following patch will take care of actually using the added function
    in place of config_with_options() at the callee which is handling a
    subrepo.
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- submodule-config.c
+++ submodule-config.c
@@ -681,7 +681,7 @@ void gitmodules_config_oid(const struct object_id *commit_oid)
 	submodule_cache_check_init(the_repository);
 
 	if (gitmodule_oid_from_commit(commit_oid, &oid, &rev)) {
-		git_config_from_blob_oid(gitmodules_cb, rev.buf,
+		git_config_from_blob_oid(the_repository, gitmodules_cb, rev.buf,
 					 &oid, the_repository);
 	}
 	strbuf_release(&rev);
