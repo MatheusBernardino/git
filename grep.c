c15d5ee85d2341f320c8f8e95f2045c87f740f24
Matheus Tavares <matheus.bernardino@usp.br>
2019-08-15 21:31:59 -0300

    grep: replace grep_read_mutex by obj_read_locks
    git-grep uses 'grep_read_mutex' to protect some object reading
    operations. But these have their own internal lock now, which ensures a
    better performance (with more parallel regions). So, let's remove the
    former and, instead, activate the latter with enable_obj_read_lock().
    
    There are two snippets which require extra attention in this conversion,
    though:
    
    - submodules' functions: besides reading the object store, those
      functions used to depend on the global the_repository for some
      internal operations. But with 935fb91 ("submodule-config: don't add
      subrepo to alternates", 2019-08-15) and 266ca55 ("grep: stop adding
      submodules to the alternates list", 2019-08-15), they no longer depend
      on it. So they are totally safe to be called by multiple threads in
      git-grep without a lock (iff the internal object reading locks are
      enabled).
    
    - textconv operations: even using object reading locks, they are still
      not thread-safe. So let's add a lock for them. This should still
      perform better then previously because the textconv critical section
      won't have to be mutual excludent with object reading, as before.
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- grep.c
+++ grep.c
@@ -1534,9 +1534,21 @@ static inline void grep_attr_unlock(void)
 }
 
 /*
- * Same as git_attr_mutex, but protecting the thread-unsafe object db access.
+ * Same as git_attr_mutex, but protecting the thread-unsafe textconv operations.
  */
-pthread_mutex_t grep_read_mutex;
+pthread_mutex_t grep_textconv_mutex;
+
+static inline void grep_textconv_lock(void)
+{
+	if (grep_use_locks)
+		pthread_mutex_lock(&grep_textconv_mutex);
+}
+
+static inline void grep_textconv_unlock(void)
+{
+	if (grep_use_locks)
+		pthread_mutex_unlock(&grep_textconv_mutex);
+}
 
 static int match_funcname(struct grep_opt *opt, struct grep_source *gs, char *bol, char *eol)
 {
@@ -1734,13 +1746,13 @@ static int fill_textconv_grep(struct repository *r,
 	}
 
 	/*
-	 * fill_textconv is not remotely thread-safe; it may load objects
-	 * behind the scenes, and it modifies the global diff tempfile
-	 * structure.
+	 * fill_textconv is not thread-safe; it may load objects behind the
+	 * scenes (but this is protected with enable_obj_read_locks()), and it
+	 * modifies the global diff tempfile structure.
 	 */
-	grep_read_lock();
+	grep_textconv_lock();
 	size = fill_textconv(r, driver, df, &buf);
-	grep_read_unlock();
+	grep_textconv_unlock();
 	free_filespec(df);
 
 	/*
@@ -2111,11 +2123,8 @@ static int grep_source_load_oid(struct grep_source *gs)
 {
 	enum object_type type;
 
-	grep_read_lock();
 	gs->buf = repo_read_object_file(gs->repo, gs->identifier, &type,
 					&gs->size);
-	grep_read_unlock();
-
 	if (!gs->buf)
 		return error(_("'%s': unable to read %s"),
 			     gs->name,
