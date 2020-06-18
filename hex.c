aa049be51cfea4ab0af6b03f7e6c7f0823f5b3d1
Matheus Tavares <matheus.bernardino@usp.br>
2020-06-17 23:03:22 -0300

    hex: make hash_to_hex_algop() and friends thread-safe
    hash_to_hex_algop() returns a static buffer, relieving callers from the
    responsibility of freeing memory after use. But the current
    implementation uses the same static data for all threads and, thus, is
    not thread-safe. We could avoid using this function and its wrappers
    in threaded code, but they are sometimes too deep in the call stack to
    be noticed or even avoided.
    
    grep.c:grep_source_load_oid(), for example, uses the thread-unsafe
    oid_to_hex() (on errors) despite being called in threaded code. And
    oid_to_hex() -- which calls hash_to_hex_algop() -- is used in many other
    places, as well:
    
    $ git grep 'oid_to_hex(' | wc -l
    818
    
    Although hash_to_hex_algop() and its wrappers don't seem to be causing
    problems out there for now (at least not reported), making them
    thread-safe makes the codebase more robust against race conditions. We
    can easily do that by replicating the static buffer in each thread's
    local storage.
    
    Original-patch-by: Fredrik Kuivinen <frekui@gmail.com>
    Signed-off-by: Fredrik Kuivinen <frekui@gmail.com>
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- hex.c
+++ hex.c
@@ -1,4 +1,5 @@
 #include "cache.h"
+#include "thread-utils.h"
 
 const signed char hexval_table[256] = {
 	 -1, -1, -1, -1, -1, -1, -1, -1,		/* 00-07 */
@@ -136,12 +137,39 @@ char *oid_to_hex_r(char *buffer, const struct object_id *oid)
 	return hash_to_hex_algop_r(buffer, oid->hash, the_hash_algo);
 }
 
+struct hexbuf_array {
+	int bufno;
+	char bufs[4][GIT_MAX_HEXSZ + 1];
+};
+
+static pthread_key_t hexbuf_array_key;
+static pthread_once_t hexbuf_array_once = PTHREAD_ONCE_INIT;
+
+static void init_hexbuf_array_key(void)
+{
+	if (pthread_key_create(&hexbuf_array_key, free))
+		die(_("failed to initialize threads' key for hash to hex conversion"));
+}
+
 char *hash_to_hex_algop(const unsigned char *hash, const struct git_hash_algo *algop)
 {
-	static int bufno;
-	static char hexbuffer[4][GIT_MAX_HEXSZ + 1];
-	bufno = (bufno + 1) % ARRAY_SIZE(hexbuffer);
-	return hash_to_hex_algop_r(hexbuffer[bufno], hash, algop);
+	struct hexbuf_array *ha;
+	void *value;
+
+	if (pthread_once(&hexbuf_array_once, init_hexbuf_array_key))
+		die(_("failed to initialize threads' key for hash to hex conversion"));
+
+	value = pthread_getspecific(hexbuf_array_key);
+	if (value) {
+		ha = (struct hexbuf_array *) value;
+	} else {
+		ha = xmalloc(sizeof(*ha));
+		if (pthread_setspecific(hexbuf_array_key, (void *)ha))
+			die(_("failed to set thread buffer for hash to hex conversion"));
+	}
+
+	ha->bufno = (ha->bufno + 1) % ARRAY_SIZE(ha->bufs);
+	return hash_to_hex_algop_r(ha->bufs[ha->bufno], hash, algop);
 }
 
 char *hash_to_hex(const unsigned char *hash)
