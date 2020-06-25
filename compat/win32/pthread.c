695b1a7b4fcc1e798af16518be97c6985518d65f
Matheus Tavares <matheus.bernardino@usp.br>
2020-06-25 00:15:53 -0300

    compat/win32/pthread: add pthread_once()
    Add pthread_once() emulation for Windows. This function provides an easy
    way to do thread-safe one-time initializations in multithreaded code. It
    will be used in the next commit to make hash_to_hex_algop()
    thread-safe.
    
    The pthread_once() implementation added comes from libav
    (https://git.libav.org/?p=libav.git), commit b22693b ("w32pthreads: Add
    pthread_once emulation", 2015-10-07). The code is licensed under
    LGPLv2.1 which is compatible with GPLv2. Only the section for support on
    Windows Vista+ has been ported, as that's the minimum version required
    to build Git for Windows.  Also, the code was modified to (1) check and
    return error codes; and (2) do not call InitOnceComplete() again after a
    successful initialization, as that results in an error.
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- compat/win32/pthread.c
+++ compat/win32/pthread.c
@@ -130,3 +130,21 @@ void *pthread_getspecific(pthread_key_t key)
 		return entry->value;
 	return NULL;
 }
+
+/* Adapted from libav's compat/w32pthreads.h. */
+int pthread_once(pthread_once_t *once_control, void (*init_routine)(void))
+{
+	BOOL pending = FALSE;
+	int ret = 0;
+
+	if (!InitOnceBeginInitialize(once_control, 0, &pending, NULL)) {
+		ret = err_win_to_posix(GetLastError());
+	} else if (pending) {
+		init_routine();
+		if (!InitOnceComplete(once_control, 0, NULL))
+			ret = err_win_to_posix(GetLastError());
+	}
+
+	/* POSIX doesn't allow pthread_once() to return EINTR */
+	return ret == EINTR ? EIO : ret;
+}
