b6c5c799d2883dc0cb0cf3f7ebbf83ba6d1c9833
Matheus Tavares <matheus.bernardino@usp.br>
2020-09-09 13:07:34 -0300

    parallel-checkout: add tests related to path collisions
    Add tests to confirm that path collisions are properly detected by
    checkout workers, both to avoid race conditions and to report colliding
    entries on clone.
    
    Original-patch-by: Jeff Hostetler <jeffhost@microsoft.com>
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- t/lib-parallel-checkout.sh
+++ t/lib-parallel-checkout.sh
@@ -9,6 +9,10 @@ test_workers_in_trace () {
 	test $(grep "child_start\[..*\] git checkout--worker" "$1" | wc -l) -eq "$2"
 }
 
+test_workers_in_event_trace () {
+	test $(grep ".event.:.child_start..*checkout--worker" "$1" | wc -l) -eq "$2"
+}
+
 # Verify that both the working tree and the index were created correctly
 verify_checkout () {
 	git -C "$1" diff-index --quiet HEAD -- &&
