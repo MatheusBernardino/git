f15f0c82fd779656dc03ce196d7c29acd63fb6be
Matheus Tavares <matheus.bernardino@usp.br>
2020-09-28 12:17:18 -0300

    packfile: fix memory leak in add_delta_base_cache()
    When add_delta_base_cache() is called with a base that is already in the
    cache, no operation is performed. But the check is done after allocating
    space for a new entry, so we end up leaking memory on the early return.
    In addition, the caller never free()'s the base as it expects the
    function to take ownership of it. But the base is not released when we
    skip insertion, so it also gets leaked. To fix these problems, move the
    allocation of a new entry further down in add_delta_base_cache(), and
    free() the base on early return.
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- packfile.c
+++ packfile.c
@@ -1474,7 +1474,7 @@ void clear_delta_base_cache(void)
 static void add_delta_base_cache(struct packed_git *p, off_t base_offset,
 	void *base, unsigned long base_size, enum object_type type)
 {
-	struct delta_base_cache_entry *ent = xmalloc(sizeof(*ent));
+	struct delta_base_cache_entry *ent;
 	struct list_head *lru, *tmp;
 
 	/*
@@ -1482,8 +1482,10 @@ static void add_delta_base_cache(struct packed_git *p, off_t base_offset,
 	 * is unpacking the same object, in unpack_entry() (since its phases I
 	 * and III might run concurrently across multiple threads).
 	 */
-	if (in_delta_base_cache(p, base_offset))
+	if (in_delta_base_cache(p, base_offset)) {
+		free(base);
 		return;
+	}
 
 	delta_base_cached += base_size;
 
@@ -1495,6 +1497,7 @@ static void add_delta_base_cache(struct packed_git *p, off_t base_offset,
 		release_delta_base_cache(f);
 	}
 
+	ent = xmalloc(sizeof(*ent));
 	ent->key.p = p;
 	ent->key.base_offset = base_offset;
 	ent->type = type;
