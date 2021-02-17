5a06223007119bd2c4b063b549e0b19a2fe60410
Matheus Tavares <matheus.bernardino@usp.br>
2021-02-17 18:02:25 -0300

    add: include magic part of pathspec on --refresh error
    When `git add --refresh <pathspec>` doesn't find any matches for the
    given pathspec, it prints an error message using the `match` field of
    the `struct pathspec_item`. However, this field doesn't contain the
    magic part of the pathspec. Instead, let's use the `original` field.
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>

--- builtin/add.c
+++ builtin/add.c
@@ -180,7 +180,7 @@ static void refresh(int verbose, const struct pathspec *pathspec)
 	for (i = 0; i < pathspec->nr; i++) {
 		if (!seen[i])
 			die(_("pathspec '%s' did not match any files"),
-			    pathspec->items[i].match);
+			    pathspec->items[i].original);
 	}
 	free(seen);
 }
